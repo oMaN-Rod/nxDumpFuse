e431a72e7e88975d742bbd0ba8329296060b3ece
Omar <omar@ojoa.life>
2021-10-28 18:49:27 -0400

    Add publish powershell script

---
+++ publish.ps1
@@ -0,0 +1,14 @@
+ $project = Join-Path $PSScriptRoot '\src\nxDumpFuse\nxDumpFuse.csproj'
+$output = Join-Path $PSScriptRoot '\src\nxDumpFuse\bin\Release\net5.0\publish\'
+ $runtimes = @(
+ "win-x64"
+ "osx-x64"
+ "linux-x64"
+ )
+    
+ # clear previous releases
+Remove-Item "$output\*" -Force -Recurse -Confirm:$true
+    
+ $runtimes | %{
+     & dotnet publish $project -c release -r $_  -o ("{0}\{1}" -f $output,$_)
+ }
